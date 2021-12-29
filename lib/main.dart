69d32034af4e49fc240b73d24f4c2184fc040a9c
khuong <vankhuong240499@gmail.com>
2021-12-29 22:53:23 +0700

    DataTypes HashMap

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'car.dart';
 
 void main() {
+  // Init old
   // var mycar = Car('Kia morning', 50);
+  // Init args
   var mycar = Car(name: 'Kia morning', yearOfProduction: 2021);
   mycar.handleEvent = () {
     // ignore: avoid_print
@@ -11,6 +14,41 @@ void main() {
   mycar.doSomeThing();
   // denial
   // mycar._doSomeThingAccess();
+
+  // List Object
+  List<Car> cars = <Car>[
+    Car(name: "Kia 1", yearOfProduction: 2020),
+    Car(name: "Kia 2", yearOfProduction: 2021),
+    Car(name: "Kia 3", yearOfProduction: 2023),
+  ];
+
+  if (kDebugMode) {
+    print(cars);
+  }
+
+  // where list
+  var car_2023 =
+      cars.where((element) => element.yearOfProduction == 2023).toList();
+  if (kDebugMode) {
+    print(car_2023);
+  }
+
+  // get list name
+  List<String> carsName = cars.map((item) => item.name).toList();
+  if (kDebugMode) {
+    print(carsName);
+  }
+
+  // Kiểu key value , nếu với nhiều value thì dùng dynamic
+  Map<String, dynamic> personA = {};
+
+  personA['name'] = 'khuong';
+  personA['age'] = 18;
+
+  if (kDebugMode) {
+    print("In File: main.dart, Line: 48 ${personA}");
+  }
+
   // ignore: avoid_print
   print(mycar);
   runApp(Center(
